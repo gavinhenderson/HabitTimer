829ed8bf4f191e08731eaf5c9d4ec90face852a4
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-02-26 20:58:13 +0000

    Initial Commit

---
+++ Shared/Habit_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TimerApp.swift
+//  Shared
+//
+//  Created by Gavin Henderson on 26/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
